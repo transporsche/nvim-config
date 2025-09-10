b05b6976badcd5f54444ec0c8d285cfbccc0c304
E14Couch <info@300watt.de>
2025-09-10 19:30:00 +0200

    matlab nochmal

--- lua/plugins/matlab.lua
+++ lua/plugins/matlab.lua
@@ -1,15 +1,20 @@
-vim.lsp.config["matlab_ls"] = {
-  -- Command and arguments to start the server.
-  cmd = { "matlab-language-server" },
-  -- Filetypes to automatically attach to.
-  filetypes = { "matlab" },
-  settings = {
-    MATLAB = {
-      installPath = "/opt/MATLAB/R2025a/bin/",
-      telemetry = false,
-    },
-  },
+return {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require("lspconfig")
+            -- lspconfig.matlab_ls.setup({})
+            lspconfig.matlab_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    MATLAB = {
+                        installPath = "/opt/MATLAB/R2025a/",
+
+                    }
+                },
+                single_file_support = true,
+            })
 }
 vim.lsp.enable("matlab_ls")
-vim.env.PATH = vim.env.PATH .. ":/opt/MATLAB/R2025a/bin/"
-return {}
+vim.env.PATH = vim.env.PATH .. ":/opt/MATLAB/R2025a/"
