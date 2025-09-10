18ec82e3333799e2b534e15380300039442b5f28
E14Couch <info@300watt.de>
2025-09-10 19:35:26 +0200

    fksdfölka

--- lua/plugins/matlab.lua
+++ lua/plugins/matlab.lua
@@ -1,20 +1,13 @@
-return {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
-            -- lspconfig.matlab_ls.setup({})
-            lspconfig.matlab_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    MATLAB = {
-                        installPath = "/opt/MATLAB/R2025a/",
-
-                    }
-                },
-                single_file_support = true,
-            })
-}
 vim.lsp.enable("matlab_ls")
 vim.env.PATH = vim.env.PATH .. ":/opt/MATLAB/R2025a/"
+return {
+  -- lspconfig.matlab_ls.setup({})
+  vim.lsp.config.matlab_ls.setup({
+    settings = {
+      MATLAB = {
+        installPath = "/opt/MATLAB/R2025a/",
+      },
+    },
+    single_file_support = true,
+  }),
+}
