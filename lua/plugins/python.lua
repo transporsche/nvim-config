ef926d519044d5a8d043fc0acbf200a9154d81e6
T14-Laptop <info@300watt.de>
2025-10-16 10:43:55 +0200

    ruff startklar gemacht

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,3 +1,39 @@
+vim.lsp.config("ruff", {
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+    },
+  },
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
+  },
+})
+
+require("lspconfig").pyright.setup({
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { "*" },
+      },
+    },
+  },
+})
+vim.lsp.enable("ruff")
 return {
   -- Manim oder so
 }
