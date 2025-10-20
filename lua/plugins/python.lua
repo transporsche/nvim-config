e2286dcdfcf800b1e472ba4fbe4b0a9bcdeed7c9
T14-Laptop <info@300watt.de>
2025-10-20 11:31:04 +0200

    RUFFI

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -23,23 +23,32 @@ require("lint").linters_by_ft = {
   python = { "ruff" },
 }
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then
-      return
-    end
-    if client.name == "ruff" then
-      -- Disable hover in favor of Pyright
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-  desc = "LSP: Disable hover capability from Ruff",
+require("lspconfig").pyright.setup({
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { "*" },
+      },
+    },
+  },
 })
 
 vim.lsp.enable("ruff")
 
 return {
-  -- Manim oder so
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  }, -- Manim oder so
 }
